f0704d3db541ba5ec926fdc78ab4da0396076530
janillejanielle <janille.janielle2@gmail.com>
2022-09-19 18:45:53 -0300

    Primeiro commit

---
+++ src/main/java/com/aula113/exemplo/Application.java
@@ -0,0 +1,13 @@
+package com.aula113.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamploApplication.class, args);
+	}
+
+}
